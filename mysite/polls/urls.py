3f7d1a6f0a38af42b20b6b95164d561b0765c70b
ssssujini99 <>
2021-09-20 22:59:16 +0900

    part3: detail, results, vote 추가

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # : /polls/
     # path()인수: route, view, (kwargs), name
+
+    path('<int:question_id>/', views.detail, name='detail'), # : /polls/5/
+    path('<int:question_id>/results/', views.results, name='results'), # : /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'), # : /polls/5/vote/
+
 ]
