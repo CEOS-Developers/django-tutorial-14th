32258fdc70b92277c83981587eb7a32f959c0770
mongus <kjyanus@gmail.com>
2021-09-21 21:51:47 +0900

    part 3 complete

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'), # 해당 url의 이름을 index로 설정
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
