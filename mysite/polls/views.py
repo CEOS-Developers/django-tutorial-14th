8b1f4f998e944701fd580dd12a4fe7d7539ddba9
Hyunjae Kim <peterhyunjae@naver.com>
2021-09-23 20:32:24 +0900

    show 404 error, by object check

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -12,12 +11,8 @@ def index(request):
 # Leave the rest of the views (detail, results, vote) unchanged
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
