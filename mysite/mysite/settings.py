c95d8480413bb92a178a608c613047793e4185ad
hywn <14hhan@gmail.com>
2021-09-24 22:20:18 +0900

    refactor: python-dotenv 이용하여 SECRET_KEY를 안전하게 보관

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@ua9j6+a@fo^$_xy1*w1pwoufiz3v-yt&v4n89ys8^pjnv7n1w'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
