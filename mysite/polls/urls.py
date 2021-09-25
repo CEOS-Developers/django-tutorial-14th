09634bd052bf46d06b1ab84b88aed782a8ac65f3
ssssujini99 <>
2021-09-25 22:59:31 +0900

    part4: generic views로 변경

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'), # : /polls/
+    path('', views.IndexView.as_view(), name='index'), # : /polls/
     # path()인수: route, view, (kwargs), name
 
-    path('<int:question_id>/', views.detail, name='detail'), # : /polls/5/
-    path('<int:question_id>/results/', views.results, name='results'), # : /polls/5/results/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # : /polls/5/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), # : /polls/5/results/
+
     path('<int:question_id>/vote/', views.vote, name='vote'), # : /polls/5/vote/
 
 ]
